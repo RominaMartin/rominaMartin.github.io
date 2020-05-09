1738a37c80b662989572f760623c9be3f55654a2
Romina Martin <romina.martin@udarealestate.com>
2020-05-09 15:54:31 +0200

    Data re-estructured

--- scripts/dist/index.dev.js
+++ scripts/dist/index.dev.js
@@ -1,21 +1,21 @@
-const MIC_OFF = "mic_off";
-const MIC_ON = "more_horiz";
-const TWITTER_BASE = "https://twitter.com/intent/tweet?text=Hello%20@rominamartinlib%20";
+"use strict";
 
-window.addEventListener("load", () => {
-  const users = document.querySelector("div.users");
-  const profile = document.querySelector("div.navbar .people");
-  const msg = document.querySelector("div.navbar .msg");
-  const msgContainer = document.querySelector("div.msg_container");
-  const bottom = document.querySelector(".bottom");
-  const sendMessageIcon = document.querySelector(".msg_container .send i");
-  const currentMessage = document.querySelector(".msg_container .send input");
-  const messagesList = document.querySelector(".msg_container .messages");
+var MIC_OFF = "mic_off";
+var MIC_ON = "more_horiz";
+var TWITTER_BASE = "https://twitter.com/intent/tweet?text=Hello%20@rominamartinlib%20";
+window.addEventListener("load", function () {
+  var users = document.querySelector("div.users");
+  var profile = document.querySelector("div.navbar .people");
+  var msg = document.querySelector("div.navbar .msg");
+  var msgContainer = document.querySelector("div.msg_container");
+  var bottom = document.querySelector(".bottom");
+  var sendMessageIcon = document.querySelector(".msg_container .send i");
+  var currentMessage = document.querySelector(".msg_container .send input");
+  var messagesList = document.querySelector(".msg_container .messages");
+  var lastTime = null;
 
-  let lastTime = null;
-
-  const sixtySecElapsed = () => {
-    let current = new Date();
+  var sixtySecElapsed = function sixtySecElapsed() {
+    var current = new Date();
 
     if (lastTime === null || (current - lastTime) / 1000 > 60) {
       lastTime = current;
@@ -26,19 +26,22 @@ window.addEventListener("load", () => {
     return false;
   };
 
-  const getFormatedTime = () => (lastTime ? `${lastTime.getHours()}:${lastTime.getMinutes()}` : "");
-  const getCurrentTime = () => {
-    let current = new Date();
-    return `${current.getHours()}:${current.getMinutes()}`;
+  var getFormatedTime = function getFormatedTime() {
+    return lastTime ? "".concat(lastTime.getHours(), ":").concat(lastTime.getMinutes()) : "";
   };
 
-  const displayMain = (name) => {
+  var getCurrentTime = function getCurrentTime() {
+    var current = new Date();
+    return "".concat(current.getHours(), ":").concat(current.getMinutes());
+  };
+
+  var displayMain = function displayMain(name) {
     document.querySelector(".main_container .active").classList.toggle("active");
-    let current = document.querySelector(`.main_container  .${name}`);
+    var current = document.querySelector(".main_container  .".concat(name));
     current.classList.add("active");
   };
 
-  const navbarDisplay = (e) => {
+  var navbarDisplay = function navbarDisplay(e) {
     if (e.target.classList.contains("people")) {
       users.classList.add("active");
       profile.classList.add("selected");
@@ -56,26 +59,24 @@ window.addEventListener("load", () => {
     }
   };
 
-  const userSelected = (e) => {
-    let prev = document.querySelector("div.users .selected");
+  var userSelected = function userSelected(e) {
+    var prev = document.querySelector("div.users .selected");
     prev.classList.remove("selected");
     prev.querySelector(".mic").innerText = MIC_OFF;
-
-    let current = e.target;
+    var current = e.target;
     current.classList.add("selected");
     current.querySelector(".mic").innerText = MIC_ON;
-
     displayMain(current.classList[1]);
   };
 
-  const sendMessage = (e) => {
+  var sendMessage = function sendMessage(e) {
     if ((e.type === "click" || e.which === 13) && currentMessage.value.length > 0) {
-      let newMessage = document.createElement("div");
+      var newMessage = document.createElement("div");
 
       if (sixtySecElapsed()) {
-        let dateTag = document.createElement("p");
-        dateTag.innerHTML = `<b>You</b> <span>${getFormatedTime()}</span>`;
-        let message = document.createElement("p");
+        var dateTag = document.createElement("p");
+        dateTag.innerHTML = "<b>You</b> <span>".concat(getFormatedTime(), "</span>");
+        var message = document.createElement("p");
         newMessage.appendChild(dateTag);
         message.innerText = currentMessage.value;
         newMessage.appendChild(message);
@@ -88,14 +89,13 @@ window.addEventListener("load", () => {
     }
   };
 
-  const addListeners = () => {
-    for (let i = 0; i < users.children.length; i++) {
+  var addListeners = function addListeners() {
+    for (var i = 0; i < users.children.length; i++) {
       users.children[i].addEventListener("click", userSelected);
     }
 
     profile.addEventListener("click", navbarDisplay);
     msg.addEventListener("click", navbarDisplay);
-
     currentMessage.addEventListener("keypress", sendMessage);
     sendMessageIcon.addEventListener("click", sendMessage);
   };
